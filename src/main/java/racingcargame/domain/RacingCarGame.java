61894daf8216265d723600010483e7ecee5a68b6
Seunghyeon Song <tmdgus1715@gmail.com>
2023-03-31 22:30:16 +0900

    feat: RacingCarGame클래스 구현

--- src/main/java/racingcargame/domain/RacingCarGame.java
+++ src/main/java/racingcargame/domain/RacingCarGame.java
@@ -1,2 +1,30 @@
-package racingcargame.domain;public class RacingCarGame {
+package racingcargame.domain;
+
+import java.util.List;
+
+public class RacingCarGame {
+
+	public static final int RANDOM_NUMBER_MIN_BOUNDARY = 0;
+	public static final int RANDOM_NUMBER_MAX_BOUNDARY = 9;
+
+	private Cars racingCars;
+
+	public RacingCarGame() {
+	}
+
+	public void init(String carNames) {
+		this.racingCars = new Cars(carNames);
+	}
+
+	public Cars getRacingCars() {
+		return racingCars;
+	}
+
+	public void gameStart() {
+		racingCars.tryMove(RANDOM_NUMBER_MIN_BOUNDARY, RANDOM_NUMBER_MAX_BOUNDARY);
+	}
+
+	public List<String> result() {
+		return racingCars.findWinner();
+	}
 }
