7667c071c0c4b66cb454b544b810e84ab5a6c464
Seunghyeon Song <tmdgus1715@gmail.com>
2023-03-31 18:58:34 +0900

    feat: 랜덤 값에 따라 이동하는 기능, Car클래스 구현

--- src/main/java/racingcargame/Car.java
+++ src/main/java/racingcargame/Car.java
@@ -1,10 +1,29 @@
 package racingcargame;
 
+import racingcargame.utill.RandomGenerator;
+import racingcargame.utill.RandomUtill;
+
 public class Car {
 
+	private final RandomUtill randomUtill = new RandomGenerator();
 	private final String name;
+	private int movement = 0;
 
 	public Car(String carName) {
 		this.name = carName;
+	}
+
+	public void tryMove(int min, int max) {
+		if (randomUtill.generateRandomNumber(min, max) > 3) {
+			move();
+		}
+	}
+
+	public int getMovement() {
+		return movement;
+	}
+
+	private void move() {
+		++movement;
 	}
 }
